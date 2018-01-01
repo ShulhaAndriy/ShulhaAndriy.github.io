0dc271972ea81430e95f63afe51da8a42b829668
ShulhaAndriy <34403155+ShulhaAndriy@users.noreply.github.com>
2018-01-01 14:16:47 +0200

    Update js.js

--- js.js
+++ js.js
@@ -1,4 +1,4 @@
-var number = Math.floor(10*Math.random()) + 1;
+var number = Math.floor(100*Math.random()) + 1;
 console.log(number);
 var limit = 10;
 var count = 9;
@@ -9,19 +9,29 @@ document.getElementById('check').onclick = function (){
 		usernum = parseInt(usernum);
 		var out = document.getElementById('out');
 		var attempt = limit - count;
-
-		if (usernum == number) {
-			out.innerHTML = 'Ура, вы угадали!!! Справились за ' + attempt + ' раз (а)';
+		
+		if (isNaN(usernum)) {
+			out.innerHTML = "Введите число от 1 до 100";
 		}
-		else if (usernum > number) {
-			out.innerHTML = 'Ваше число больше';
-		}
-		else if (usernum<number){
-			out.innerHTML = 'Ваше число меньше';
-		}
-
-		document.getElementById('count').innerHTML = 'Осталось попыток: '+count+' из '+limit;
-		count = count - 1;
+		else {
+			if (usernum > 0 && usernum < 100) {
+				if (usernum == number) {
+				out.innerHTML = 'Ура, вы угадали!!! Справились за ' + attempt + ' раз (а)';
+				check.disabled = true;
+				}
+				else if (usernum > number) {
+				out.innerHTML = 'Ваше число больше';
+				}
+				else if (usernum<number){
+				out.innerHTML = 'Ваше число меньше';
+				}
+				document.getElementById('count').innerHTML = 'Осталось попыток: '+count+' из '+limit;
+				count = count - 1;
+			}
+			else {
+				out.innerHTML = 'Введите число от 1 до 100';
+			}
+		}	
 	}
 	else {
 		alert ('Попытки закончены, вы проиграли. Попробуйте еще. Загаданое число: ' + number);
